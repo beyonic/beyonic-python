c83a192a9d5509edf0927cc03cbe78e1ef342402
Luke Kyohere <KayLuke@users.noreply.github.com>
2015-01-01 20:07:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "0.1.2a1"
+VERSION = "0.1.3a1"
 
 setup(
     name="beyonic",
