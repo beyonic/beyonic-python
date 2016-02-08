51bea6a544c93d89b6661ee8eb8f0d95bad02284
Liyosi Collins <collinskivale@gmail.com>
2016-02-08 12:54:45 +0300

    Accounts API Wrapper doc string
    - update doc string

--- beyonic/apis/account.py
+++ beyonic/apis/account.py
@@ -3,6 +3,6 @@ from beyonic.apis.abstract_api import AbstractAPI
 
 class Account(AbstractAPI):
     """
-    Collection request api wrapper class
+    Account api wrapper class
     """
     _method_path = 'accounts'
